38ddd2980b7ac3336623db50eac75cee9f04c923
Wali-Youssef <117752075+Wali-Youssef@users.noreply.github.com>
2024-03-25 09:19:36 +0100

    Refactor login button in login.vue component

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import { supabase, user } from '../supabase';
 </script>
+
 <template>
-<div>
-    <button v-if="user" @pointerdown="supabase.auth.signOut()">
-        Se déconnecter ({{user.email}})
+  <div>
+    <button v-if="user" @click="supabase.auth.signOut()">
+      Se déconnecter ({{user.email}})
     </button>
-    <button v-else @pointerdown="supabase.auth.signInWithOAuth({provider: 'github',options: {redirectTo: "https://portfolio.youssefwali.fr/projet/edit"}})">
-        Se connecter avec Github
+    <button v-else @click="supabase.auth.signInWithOAuth({ provider: 'github', options: { redirectTo: 'https://portfolio.youssefwali.fr/projet/edit' } })">
+      Se connecter avec Github
     </button>
-</div>
+  </div>
 </template>
